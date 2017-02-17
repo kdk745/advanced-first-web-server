b6e833fff3e3d363d9ef1578a692f0e09ca979e9
Kayne Khoury <kaynekhoury1@gmail.com>
2017-02-17 17:49:24 -0600

    added express functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Your server code here...
+import express from 'express';
+
+
+// create a new instance of express
+const app = express();
+
+
+// declare the route
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+// set the port
+const PORT = 3001;
+
+// listen to request
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error!', err);
+
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
